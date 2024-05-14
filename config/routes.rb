e5fb2f17b9b84b1e6453cc149e13fea8f0e5b725
Radamés <radames.idiomas@gmail.com>
2024-05-14 18:52:51 -0300

    Criando a página index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  root 'home#index'
   resources :posts
   get "/articles", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# para visualizar as rotas do projeto
+# http://127.0.0.1:3000/rails/info/routes
